d0a8f16c1d36a40fdda4d681540530c2a0a82549
varvara-sergeeva <raunuperetemheru@gmail.com>
2023-01-22 18:21:49 +0300

    product card mixin on pug

--- src/blocks/modules/blocks/blocks.js
+++ src/blocks/modules/blocks/blocks.js
@@ -32,4 +32,50 @@ document.addEventListener("DOMContentLoaded", function () {
 	console.log(getCurrentDate(new Date()));
 
 	document.getElementById('dateOutput').innerHTML = fullCurrentDate;
+
+
+	//- СЧЕТЧИК ДЛЯ карточек товара
+
+	const quantityProducts = document.querySelectorAll('.quantity-product');
+	const cartButtons = document.querySelectorAll('.product-card__cart-btn');
+
+	if (cartButtons) {
+		cartButtons.forEach(cartButton => {
+			cartButton.addEventListener("click", e => {
+				cartButton.style.visibility = 'hidden';
+			})
+		})
+	}
+
+	if (quantityProducts) {
+		quantityProducts.forEach(quantityProduct => {
+			quantityProduct.addEventListener("click", e => {
+				const target = e.target;
+				const targetButton = quantityProduct.previousElementSibling;
+
+				//- событие пихаем в переменную, а потом выясняем что за событие произошло
+				if (target.closest(".quantity-product__button")) {
+					//- находим значение инпута
+					let value = parseInt(target.closest(".quantity-product").querySelector("input").value);
+					//- если жмем на плюс, то увеличиваем значение, если нет, то вычитаем значение СОДЕРЖИТ БЕЗ ТОЧКИ
+					if (target.classList.contains("quantity-product__plus")) {
+						value++;
+					} else {
+						--value;
+					}
+					//- ограничиваем значения в меньшую и большую сторону
+					if (value <= 0) {
+						value = 0;
+						targetButton.style.visibility = 'visible';
+					}
+					if (value >= 10) {
+						value = 10;
+					}
+
+					//- вписываем в инпут значение
+					target.closest(".quantity-product").querySelector("input").value = value;
+				}
+			})
+		})
+	}
 });
--- src/blocks/modules/flipCard/flipCard.js
+++ src/blocks/modules/flipCard/flipCard.js
@@ -1,31 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const cardButtons = document.querySelectorAll('.card-front__button');
-  const cards = document.querySelectorAll('.card');
+	const cardButtons = document.querySelectorAll('.card-front__button');
+	const cards = document.querySelectorAll('.card');
 
-  for (let cardBtn of cardButtons) {
-    cardBtn.addEventListener("click", function (e) {
-      e.preventDefault();
-      for (let card of cards) {
-        card.classList.remove('active');
-        console.log('removed')
-      };
-      // создаем переменную для текущей кнопки
-      const currentButton = cardBtn.dataset.filter;
-      activeAnimationCard(currentButton, cards);
-    })
-  }
+	for (let cardBtn of cardButtons) {
+		cardBtn.addEventListener("click", function (e) {
+			e.preventDefault();
+			for (let card of cards) {
+				card.classList.remove('active');
+				console.log('removed')
+			};
+			// создаем переменную для текущей кнопки
+			const currentButton = cardBtn.dataset.filter;
+			activeAnimationCard(currentButton, cards);
+		})
+	}
 
-  // функция по соответствию
-  function activeAnimationCard(pair, items) {
-    items.forEach((item) => {
-      const isItemIncorrect = !item.classList.contains(pair);
-      if (isItemIncorrect) {
-        item.classList.remove('active')
-        console.log('removed class active')
-      } else {
-        item.classList.toggle('active')
-      }
-    })
-  }
+	// функция по соответствию
+	function activeAnimationCard(pair, items) {
+		items.forEach((item) => {
+			const isItemIncorrect = !item.classList.contains(pair);
+			if (isItemIncorrect) {
+				item.classList.remove('active')
+				console.log('removed class active')
+			} else {
+				item.classList.toggle('active')
+			}
+		})
+	}
 
 });
+
+
+
+
+
+//const quantityProducts = document.querySelectorAll('.quantity-product');
+
+//if (quantityProducts) {
+//	quantityProducts.forEach(quantityProduct => {
+//		quantityProduct.addEventListener('click', e => {
+//			const target = e.target;
+
+//			if (target.closest('.quantity-product__button')) {
+//				let value = parseInt(target.closest('.quantity-product').querySelector('input').value);
+
+//				if (target.classList.contains('quantity-product__plus')) {
+//					value++;
+//				} else {
+//					--value;
+//				}
+
+//				if (value <= 0) {
+//					value = 0;
+//					target.closest('.quantity-product').querySelector('.counter__button_minus').classList.add('disabled')
+//				} else {
+//					target.closest('.quantity-product').querySelector('.counter__button_minus').classList.remove('disabled')
+//				}
+
+//				target.closest('.quantity-product').querySelector('input').value = value;
+//			}
+//		})
+//	})
+//}
